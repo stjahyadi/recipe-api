8caf5eb9bf5e10beef2caaf232f07bf4947896a4
Sean Young Tjahyadi <stjahyadi@bol.com>
2023-06-14 23:17:08 +0200

    first commit

---
+++ src/main/java/com/recipe/recipefavourite/RecipeFavouriteApplication.java
@@ -0,0 +1,13 @@
+package com.recipe.recipefavourite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeFavouriteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeFavouriteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/recipe/recipefavourite/RecipeFavouriteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recipe.recipefavourite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeFavouriteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
